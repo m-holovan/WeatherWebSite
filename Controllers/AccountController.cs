3ff4b5c48d9881da3e2ccd0ed616e0f162cfdb31
m-holovan <nikita2003n@gmail.com>
2023-07-12 20:25:34 +0300

    add logic for register and login methods

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,20 +1,78 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Security.Claims;
 using WeatherApplication.Models;
 
 namespace WeatherApplication.Controllers
 {
     public class AccountController : Controller
     {
+        private ApplicationDbContext db;
+        public AccountController(ApplicationDbContext context)
+        {
+            db = context;
+        }
+
         [HttpGet]
         public IActionResult Register() => View();
 
         [HttpPost]
-        public IActionResult Register(RegisterModel model) => View(model);
+        public async Task<IActionResult> Register(RegisterModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = await db.Users.FirstOrDefaultAsync(u => u.Email == model.Email);
+                if (user == null)
+                {
+                    db.Users.Add(new User { Name = model.Name, Surname = model.Surname, Email = model.Email, Password = model.Password });
+                    await db.SaveChangesAsync();
+                    await Authenticate(model.Email); // аутентификация
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            return View(model);
+        }
 
         [HttpGet]
         public IActionResult Login() => View();
 
         [HttpPost]
-        public IActionResult Login(LoginModel model) => View(model);
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = await db.Users.FirstOrDefaultAsync(u => u.Email == model.Email && u.Password == model.Password);
+
+                if (user != null)
+                {
+                    await Authenticate(model.Email);
+
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            return View(model);
+        }
+
+        private async Task Authenticate(string userEmail)
+        {
+            // создаем один claim
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimsIdentity.DefaultNameClaimType, userEmail)
+            };
+            // создаем объект ClaimsIdentity
+            ClaimsIdentity id = new ClaimsIdentity(claims, "ApplicationCookie", ClaimsIdentity.DefaultNameClaimType, ClaimsIdentity.DefaultRoleClaimType);
+            // установка аутентификационных куки
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(id));
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Login", "Account");
+        }
     }
 }
